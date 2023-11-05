6c17f73bf6c4650929b5df2d64a54b6b6c9d13de
AlmogEisen <almog.eisen2@gmail.com>
2023-11-05 17:26:46 +0200

    Added Communication files
    Server.go and Client.go files have been added.
    We might need to divide the client and server.go into different branches in the future.

---
+++ Server.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func handleConnection(conn net.Conn) {
+	fmt.Println(conn.RemoteAddr(), "is connected")
+}
+
+func main() {
+	addr := "192.168.50.191:12345"
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer listener.Close()
+
+	fmt.Printf("Server is listening on %s...\n", addr)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+			continue
+		}
+		go handleConnection(conn)
+	}
+}
