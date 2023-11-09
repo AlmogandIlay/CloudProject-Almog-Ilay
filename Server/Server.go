076c58359f2562603c6e1267794e3a9feeee0e45
AlmogEisen <almog.eisen2@gmail.com>
2023-11-09 19:30:42 +0200

    Server.go of the first POC has been uploaded
    Here is the Server.go of the first POC we had

---
+++ Server/Server.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"strconv"
+)
+
+func handleConnection(conn net.Conn) {
+	const SizeDigits = 13
+	fmt.Println(conn.RemoteAddr(), "is connected")
+	buf := make([]byte, SizeDigits)
+	_, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(buf)
+	var size int
+	var convert string
+	for i := 0; i < SizeDigits; i++ {
+		if buf[i] == 0 {
+			break
+		}
+		convert += string(buf[i])
+	}
+
+	size, _ = strconv.Atoi(convert)
+	fmt.Println("Size of the given file is ", size)
+	fmt.Printf("%T", buf[0])
+	buf = make([]byte, size)
+	_, err = conn.Read(buf)
+	if err != nil {
+		fmt.Println(err)
+	}
+	println("File content is:")
+	println(string(buf))
+}
+
+func main() {
+	addr := "192.168.50.191:12345"
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer listener.Close()
+
+	fmt.Printf("Server is listening on %s...\n", addr)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+			continue
+		}
+		go handleConnection(conn)
+	}
+}
