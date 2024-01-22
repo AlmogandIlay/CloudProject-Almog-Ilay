26bf55cce8b5fa485b20e3a005d33e7c722a6be6
AlmogEisen <almog.eisen2@gmail.com>
2024-01-22 22:47:35 +0200

    No need connection package

--- Menu/menu.go
+++ Menu/menu.go
@@ -1,15 +1,33 @@
 package Menu
 
 import (
+	"errors"
 	"fmt"
+	"net"
+)
+
+const (
+	ip_addr = "192.168.50.191:12345"
 )
 
 type CLI struct {
 	prompt string
+	socket net.Conn
 }
 
-func NewCLI() *CLI {
-	return &CLI{prompt: ">> "}
+func NewCLI() (*CLI, error) {
+	var err error
+	var cli CLI
+
+	cli.prompt = ">>"
+	cli.socket, err = net.Dial("tcp", ip_addr)
+	if err != nil {
+
+		return nil, errors.New(fmt.Sprintf("There has been an error connecting to the server.\nPlease check your connection and try again.\nIf it doesn't work contact the developers and send them this error message:\n%s", err.Error()))
+	}
+
+	return &cli, nil
+
 }
 
 // Prints the program startup intro
