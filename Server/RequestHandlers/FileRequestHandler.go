832371650ff1e4c3c867535b1c3a1e7ac7077c52
AlmogEisen <almog.eisen2@gmail.com>
2024-01-30 00:53:36 +0200

    Added request types

---
+++ Server/RequestHandlers/FileRequestHandler.go
@@ -0,0 +1,23 @@
+package RequestHandlers
+
+import "CloudDrive/Server/RequestHandlers/Requests"
+
+type FileRequestHandler struct{}
+
+func (loginHandler *FileRequestHandler) HandleRequest(info Requests.RequestInfo) ResponeInfo {
+	switch info.Type {
+	case Requests.ChangeDirectoryRequest:
+		return loginHandler.HandleLogin(info)
+	case Requests.CreateFileRequest:
+		return loginHandler.HandleSignup(info)
+	case Requests.CreateFolderRequest:
+		// TODO
+	case Requests.DeleteFileRequest:
+		// TODO
+	case Requests.DeleteFolderRequest:
+		// TODO
+	default:
+		return loginHandler.HandleError(info)
+	}
+
+}
--- Server/RequestHandlers/Requests/RequestInfo.go
+++ Server/RequestHandlers/Requests/RequestInfo.go
@@ -5,9 +5,17 @@ import "encoding/json"
 type RequestType int
 
 const (
+	// Authorization requests
 	LoginRequest  RequestType = 101
 	SignupRequest RequestType = 102
-	// ...
+
+	// File requests
+	ChangeDirectoryRequest RequestType = 301
+	CreateFileRequest      RequestType = 302
+	CreateFolderRequest    RequestType = 303
+	DeleteFileRequest      RequestType = 304
+	DeleteFolderRequest    RequestType = 305
+
 	ErrorRequest RequestType = 999
 )
 
