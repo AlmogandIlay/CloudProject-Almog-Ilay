ecc0328022fca710e211f2dbdc7ea7ed32c0a91a
AlmogEisen <almog.eisen2@gmail.com>
2024-03-30 22:03:51 +0300

    Update Client.go

--- Client.go
+++ Client.go
@@ -4,13 +4,16 @@ import (
 	Menu "client/Menu"
 	"fmt"
 	"log"
+	"os"
 )
 
 func main() {
 	cli, err := Menu.NewCLI()
 	if err != nil { // If server connection fails
-		fmt.Printf("\n") // Divide connection error from other texts
-		log.Fatal(err)
+		log.Printf("\n%v\n\n", err)
+		fmt.Println("Press 'Enter' to exit...")
+		fmt.Scanln()
+		os.Exit(1)
 	}
 
 	cli.PrintStartup()
